b5d00312226053e29ff6d7b202e144be0ce53fe8
matti <matti.soucaille@ensta-bretagne.org>
2024-03-20 16:42:24 +0100

    Regulateur fait

--- src/custom/include/custom/nodeRegulateur.h
+++ src/custom/include/custom/nodeRegulateur.h
@@ -6,14 +6,41 @@
 #define PROJET_RC_NODEREGULATEUR_H
 
 
-class nodeRegulateur : public rclcpp::Node {
-public:
-    string etatTopicName = "";
-    string positionTopicName = "";
-    manualTopicName = "";
-    commandTopicName = "";
+class nodeRegulateur {
 
-    void callbackTimer()
+    /*
+     * Subscriber data and function
+     */
+    // Bool
+    bool lastStateBool = true;
+    bool lastManualBool = true;
+    // Real position
+    float real_x = 0;
+    float real_y = 0;
+    float real_z = 0;
+    float real_psi = 0;
+    float real_theta = 0;
+    float real_phi = 0;
+    // Target position
+    float target_x = 0;
+    float target_y = 0;
+    float target_z = 0;
+    float target_psi = 0;
+    float target_theta = 0;
+    float target_phi = 0;
+    // Fonction callback
+    void callbackSubscriptionReceiveRealPosition(const geometry_msgs::msg::PoseStamped &realPosition_msg);
+    void callbackSubscriptionReceiveTargetPosition(const geometry_msgs::msg::PoseStamped &targetPosition_msg);
+    void callbackSubscriptionReceiveStateBool(const std_msgs::msg::Bool &stateBool_msg);
+    void callbackSubscriptionReceiveManualBool(const std_msgs::msg::Bool &manualBool_msg);
+
+    /*
+     * Publisher data and function
+     */
+    // Twist commande
+    geometry_msgs::msg::Twist cmdTwist_msg;
+    // Fonction callback
+    void timerSendCmd(void);
 };
 
 
--- src/custom/src/nodeRegulateur.cpp
+++ src/custom/src/nodeRegulateur.cpp
@@ -5,12 +5,50 @@
 #include "../include/custom/nodeRegulateur.h"
 
 nodeRegulateur::nodeRegulateur() : Node("nodeRegulateur") {
+    // Timer
+    timerSendCmd_ = this->create_wall_timer(100ms, std::bind(&TurretSimulated::timerSendCmd, this)); // Timer d'envoie de la commande aux actionneurs
 
+    // Publisher
+    publisherSendCmd_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd", 10);
 
-    timer_ = this->create_wall_timer(100ms, std::bind(&TurretSimulated::timer_callbackPublisher, this));
-    publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("turret3D", 10);
+    // Subscriber
+    subscriberReceiveRealPosition = this->create_subscription<geometry_msgs::msg::PoseStamped>("realPosition", 10, std::bind(&nodeRegulateur::callbackSubscriptionReceiveRealPosition, this, _1)); ; // Subscriber reçoit la position du robot
+    subscriberReceiveTargetPosition = this->create_subscription<geometry_msgs::msg::PoseStamped>("targetPosition", 10, std::bind(&nodeRegulateur::callbackSubscriptionReceiveTargetPosition, this, _1));; // Subscriber reçoit la position cible
+    subscriberReceiveStateBool = this->create_subscription<std_msgs::msg::Bool>("stateBool", 10, std::bind(&nodeRegulateur::callbackSubscriptionReceiveStateBool, this, _1));; // Subscriber reçoit de la part du node Mission si il doit s'activer
+    subscriberReceiveManualBool = this->create_subscription<std_msgs::msg::Bool>("manualBool", 10, std::bind(&nodeRegulateur::callbackSubscriptionReceiveManualBool, this, _1));; // Subscriber reçoit de la part du node Client si il doit s'activer
+}
 
-    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-    tf_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
+void nodeRegulateur::callbackSubscriptionReceiveRealPosition(const geometry_msgs::msg::PoseStamped &realPosition_msg){
+
+    this->real_x = realPosition_msg.pose.position.x;
+    this->real_y = realPosition_msg.pose.position.y;
+    this->real_z = realPosition_msg.pose.position.z;
+    this->real_psi = tf2::getYaw(realPosition_msg.pose.orientation); // Pitch
+    this->real_phi = tf2::getYaw(realPosition_msg.pose.orientation); // Yaw
+    this->real_theta = tf2::getYaw(realPosition_msg.pose.orientation); // Theta
+
+}
+
+void nodeRegulateur::callbackSubscriptionReceiveTargetPosition(const geometry_msgs::msg::PoseStamped &targetPosition_msg){
+
+    this->target_x = targetPosition_msg.pose.position.x;
+    this->target_y = targetPosition_msg.pose.position.y;
+    this->target_z = targetPosition_msg.pose.position.z;
+    this->target_psi = tf2::getYaw(targetPosition_msg.pose.orientation); // Pitch
+    this->target_phi = tf2::getYaw(targetPosition_msg.pose.orientation); // Yaw
+    this->target_theta = tf2::getYaw(targetPosition_msg.pose.orientation); // Theta
+
+}
+
+void nodeRegulateur::callbackSubscriptionReceiveStateBool(const std_msgs::msg::Bool &stateBool_msg){
+    this->lastStateBool = stateBool_msg.data;
+}
+
+void nodeRegulateur::callbackSubscriptionReceiveManualBool(const std_msgs::msg::Bool &manualBool_msg){
+    this->lastManualBool = manualBool_msg.data;
+}
+
+void timerSendCmd(){
+
+    publisherSendCmd_->publish(cmdTwist_msg);
 }
